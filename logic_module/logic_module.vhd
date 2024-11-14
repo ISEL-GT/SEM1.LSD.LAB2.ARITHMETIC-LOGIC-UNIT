6d2a3806ea23163633965fbd4adad8e8201de569
1xSh4dowx1 <duarte.afonso.rodrigues@gmail.com>
2024-11-14 13:21:09 +0000

    add the file flags

--- logic_module/logic_module.vhd
+++ logic_module/logic_module.vhd
@@ -131,7 +131,7 @@ begin
 			carry_out => carry_logical_shift_left
 		);
 		
-		-- Instantiation of the nand_gate
+	-- Instantiation of the nand_gate
 	instance_logical_nand_gate : nand_gate
 	
 		port map (
@@ -142,7 +142,7 @@ begin
 			
 		);
 		
-			-- Instantiation of the mux_4inputs
+	-- Instantiation of the mux_4inputs
 	instance_mux_4inputs : mux_4inputs
 	
 		port map (
